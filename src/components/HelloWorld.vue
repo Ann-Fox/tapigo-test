3758088f7ceda951bd266b41abe7abba0c344cb3
Ann-Fox <fx8565@mail.ru>
2025-04-09 19:34:24 +0500

    feat: удалены ненужные сомпоненты, создан спиок задач

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,68 @@
 <script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
-})
+import { ref } from 'vue'
+
+const items = ref([
+  { "id": 1, "title": "Задача 1", "done": false },
+  { "id": 2, "title": "Задача 2", "done": false },
+  { "id": 3, "title": "Задача 3", "done": false },
+  { "id": 4, "title": "Задача 4", "done": false },
+  { "id": 5, "title": "Задача 5", "done": false }
+])
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
+ <div>
+  <ul>
+    <li v-for="item in items" class="item" :key="item.id">
+      <input class="checkbox-input" type="checkbox" v-model="item.done">
+      <label class="checkbox-label" :class="{'completed': item.done}">{{ item.title }}</label>
+    </li>
+  </ul>
+ </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
+.completed {
+  text-decoration: line-through;
+}
+.item {
+  margin-top: 5px;
+  width: fit-content;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.checkbox-label {
+  padding-left: 5px;
+}
+
+.checkbox-input {
+  appearance: none;
   position: relative;
-  top: -10px;
+  width: 30px;
+  height: 30px;
+  background: #16d686;
+  box-shadow: inset 0 0 5px rgb(0 0 0 / 0.2);
+  border-radius: 10px;
+  border: 1px solid #FFFFFF;
+  transition: 500ms;
 }
 
-h3 {
-  font-size: 1.2rem;
+.checkbox-input:checked::after {
+  width: 30px;
+  height: 30px;
+  transition: 500ms;
 }
 
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+.checkbox-input::after {
+  content: "";
+  position: absolute;
+  width: 0px;
+  height: 0px;
+  font-size: 30px;
+  background-image: url("./icons/check.svg");
+  background-repeat: no-repeat;
+  transition: 500ms;
 }
 </style>
