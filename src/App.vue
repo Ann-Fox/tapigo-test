07afe76d5c61cfc729098d3f07a1efc78d412a4a
Ann-Fox <fx8565@mail.ru>
2025-04-18 18:25:01 +0500

    feat: add  show/hide completed tasks

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 let id = 0
 
+const completed = ref(false)
 const newTodo = ref('')
 const todos = ref([
   { id: id++, text: 'Learning JavaScript', done: true },
   { id: id++, text: 'Learning Vue.js 3', done: true },
   { id: id++, text: 'To be happy', done: false },
 ])
+
+const dotosFilter = computed(() => {
+  return completed.value ? todos.value.filter((t) => !t.done) : todos.value
+})
 
 function addTask() {
   todos.value.push({ id: id++, text: newTodo.value })
@@ -31,13 +36,13 @@ function removeTask(todo) {
       <button @click="addTask">Add task</button>
     </div>
     <ul>
-      <li v-for="todo in todos" :key="todo.id" :class="{ 'done': todo.done }">
+      <li v-for="todo in dotosFilter" :key="todo.id" :class="{ 'done': todo.done }">
         <input type="checkbox" name="" v-model="todo.done">
         {{ todo.text }}
         <button @click="removeTask(todo)">del</button>
       </li>
     </ul>
-    <button>show\hide all done tasks</button>
+    <button @click="completed = !completed">{{ completed ? 'show all tasks' : 'hide done tasks' }}</button>
   </main>
 </template>
 
