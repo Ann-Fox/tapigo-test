7aa661ea81b4872a495d25219cd4051f33b7e42c
Ann-Fox <fx8565@mail.ru>
2025-04-18 12:46:53 +0500

    feat: add start layout

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,32 @@
 <script setup>
-import TaskList from './components/TaskList.vue';
+import { ref } from 'vue'
+
+const todos = ref([
+  { id: 0, text: 'Learning JavaScript' },
+  { id: 1, text: 'Learning Vue.js 3' },
+  { id: 2, text: 'To be happy' },
+])
 </script>
 
 <template>
   <header>
-    <h1>Task list Tapigo</h1>
+    <h1>TODO LIST</h1>
   </header>
 
   <main>
-    <TaskList />
+    <div>
+      <input type="text">
+      <button>Add task</button>
+    </div>
+    <ul>
+      <li v-for="todo in todos">
+        <input type="checkbox" name="" id="">
+        {{ todo.text }}
+        <button>delete task</button>
+      </li>
+    </ul>
+    <button>show\hide all done tasks</button>
   </main>
 </template>
 
-<style scoped>
-header {
-  display: flex;
-  justify-content: center;
-}
-</style>
+<style scoped></style>
